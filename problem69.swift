b77b8f27c0c652abe738b34448596fc9e804bf7b
Mateusz Lesniak <mateuszlesniak@tango.agency>
2022-06-28 16:47:55 +0200

    Adds problem 69

---
+++ problem69.swift
@@ -0,0 +1,29 @@
+// https://projecteuler.net/problem=69
+
+import Foundation
+
+var globalFactors: [Int: Set<Int>] = [:]
+
+let primes: Set<Int> = {
+    let limit = 999999
+    var primes: Set<Int> = .init(2...limit)
+    for prime in 2...limit where primes.contains(prime) {
+        stride(from: prime + prime, to: limit + 1, by: prime).forEach {
+            primes.remove($0)
+        }
+    }
+    return primes
+}()
+
+let start = CFAbsoluteTimeGetCurrent()
+
+let max = 1000000
+var result = 1
+for prime in primes.sorted() {
+    guard result * prime <= max else { break }
+    result *= prime
+}
+print(result)
+
+let diff = CFAbsoluteTimeGetCurrent() - start
+print("\(#function) Took \(diff) seconds")
